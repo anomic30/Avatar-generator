6d18d01c0964f94d8b891eab3465836ad5385070
[Anom Chakravorty] <anomchakravorty3008@gmail.com>
2021-07-04 01:24:59 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-import Navbar from './Components/Navbar';
 import Home from './Components/Home';
 import Footer from './Components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
       <Home />
       <Footer/>
     </div>
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -15,48 +15,53 @@ const Home = () => {
         setSeed(x);
     }
 
-    function downloadImage(){
+    function downloadImage() {
         Axios({
-        method: "get",
-        url: `https://avatars.dicebear.com/api/${sprite}/${seed}.svg`,
-        responseType: "arraybuffer"
+            method: "get",
+            url: `https://avatars.dicebear.com/api/${sprite}/${seed}.svg`,
+            responseType: "arraybuffer"
         })
-        .then((response) => {
-            var link = document.createElement("a");
-            link.href = window.URL.createObjectURL(
-            new Blob([response.data], { type: "application/octet-stream" })
-            );
-            link.download = `${seed}.svg`;
-        
-            document.body.appendChild(link);
-        
-            link.click();
-            setTimeout(function () {
-            window.URL.revokeObjectURL(link);
-            }, 200);
-        })
-        .catch((error) => {});
+            .then((response) => {
+                var link = document.createElement("a");
+                link.href = window.URL.createObjectURL(
+                    new Blob([response.data], { type: "application/octet-stream" })
+                );
+                link.download = `${seed}.svg`;
+
+                document.body.appendChild(link);
+
+                link.click();
+                setTimeout(function () {
+                    window.URL.revokeObjectURL(link);
+                }, 200);
+            })
+            .catch((error) => { });
     }
 
     return (
-        <div className="home">
-            <div className="btns">
-                <button onClick={()=>{ handleSprite("avataaars") }}>Human</button>
-                <button onClick={()=>{handleSprite("male")}}>Male</button>
-                <button onClick={()=>{handleSprite("female")}}>Female</button>
-                <button onClick={()=>{handleSprite("identicon")}}>Identicon</button>
-                <button onClick={()=>{handleSprite("bottts")}}>Bots</button>
-                <button onClick={()=>{handleSprite("jdenticon") }}>Vector</button>
-                <button onClick={()=>{handleSprite("gridy") }}>Gridy</button>
-                <button onClick={()=>{handleSprite("micah") }}>Avatars</button>
-                <button onClick={()=>{handleSprite("human")}}>Pixel</button>
+        <div className="container">
+            <div className="nav">
+                <p>Random Avatar Generator</p>
             </div>
-            <div className="avatar">
-                <img src={`https://avatars.dicebear.com/api/${sprite}/${seed}.svg`} alt="Sprite"/>
-            </div>
-            <div className="generate">
-                <button id="gen" onClick={() => { handleGenerate() }}>Generate</button>
-                <button id= "down" onClick={()=>{downloadImage()}}>Download</button>
+            <div className="home">
+                <div className="btns">
+                    <button onClick={() => { handleSprite("avataaars") }}>Human</button>
+                    <button onClick={() => { handleSprite("male") }}>Male</button>
+                    <button onClick={() => { handleSprite("female") }}>Female</button>
+                    <button onClick={() => { handleSprite("identicon") }}>Identi</button>
+                    <button onClick={() => { handleSprite("bottts") }}>Bots</button>
+                    <button onClick={() => { handleSprite("jdenticon") }}>Vector</button>
+                    <button onClick={() => { handleSprite("gridy") }}>Gridy</button>
+                    <button onClick={() => { handleSprite("micah") }}>Avatars</button>
+                    <button onClick={() => { handleSprite("human") }}>Pixel</button>
+                </div>
+                <div className="avatar">
+                    <img src={`https://avatars.dicebear.com/api/${sprite}/${seed}.svg`} alt="Sprite" />
+                </div>
+                <div className="generate">
+                    <button id="gen" onClick={() => { handleGenerate() }}>Generate</button>
+                    <button id="down" onClick={() => { downloadImage() }}>Download</button>
+                </div>
             </div>
         </div>
     )
